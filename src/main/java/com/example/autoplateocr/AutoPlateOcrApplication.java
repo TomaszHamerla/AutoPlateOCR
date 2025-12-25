478162579238ba293838d334459a9fa893d988af
unknown <tomaszhamerla@interia.pl>
2025-12-25 12:44:03 +0100

    init

---
+++ src/main/java/com/example/autoplateocr/AutoPlateOcrApplication.java
@@ -0,0 +1,13 @@
+package com.example.autoplateocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPlateOcrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoPlateOcrApplication.class, args);
+    }
+
+}
